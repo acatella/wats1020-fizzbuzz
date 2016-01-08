2814f3267a9ded0a5b39a7fab253e5af6ceede9b
Afi C. Clark <aficclark@gmail.com>
2016-01-07 18:58:05 -0800

    moved notes to the top to make the function look more clear

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,22 @@
 //creating a function so user can choose numbers and limit
+//for loop to run through all number from 1 to the limit argument
+//start by creating an empty string
+//if i is divisble by the num1 argument, add 'Fizz' to our string
+//same thing for num2 argument
+//if the output string is unchanged it will be empty, which equates to false in boolean
+//here we call the function using 3,5, and 20 as our arguments
+
 function fizzBuzz(num1,num2,limit) {
-  //for loop to run through all number from 1 to the limit argument
   for (i=1; i<=limit; i++) {
-    //start by creating an empty string
     var output = "";
-    //if i is divisble by the num1 argument, add 'Fizz' to our string
     if (i%num1 == 0) {
       output += 'Fizz'
     }
-    //same thing for num2 argument
+  
     if (i%num2 == 0) {
       output += 'Buzz'
     }
     
-    //if the output string is unchanged it will be empty, which equates to false in boolean
     if (!output) {
       output += i
     }
@@ -22,5 +25,5 @@ function fizzBuzz(num1,num2,limit) {
   }
 }
 
-//here we call the function using 3,5, and 20 as our arguments
+
 fizzBuzz(3,5,20)
