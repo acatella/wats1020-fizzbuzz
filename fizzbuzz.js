e8a8f4e7bf8f3cbfa0e655b572fc1d40fa8fc89d
acatella <aficclark@gmail.com>
2016-01-09 16:18:28 -0800

    finished styling, added <li> for fizz items

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,25 +2,38 @@
 //for loop to run through all number from 1 to the limit argument
 //start by creating an empty string
 //if i is divisble by the num1 argument, add 'Fizz' to our string
-//same thing for num2 argument
+//same thing for num2 argument, but with 'Buzz'
 //if the output string is unchanged it will be empty, which equates to false in boolean
-//here we call the function using 3,5, and 20 as our arguments
+//then we call the function using 3,5, and 20 as our arguments
 
 function fizzBuzz(num1,num2,limit) {
+  //code to keep button black
+  var button = document.getElementById('js-trigger');
+  button.setAttribute('class','blackBtn');
+
   for (i=1; i<=limit; i++) {
     var output = "";
+    var outputTag = "";
     if (i%num1 == 0) {
       output += 'Fizz'
+      outputTag = "<li class='purple'>" + output + "</li>"
     }
 
     if (i%num2 == 0) {
       output += 'Buzz'
+      if (output[0] == 'F')
+        outputTag = "<li class='red'>" + output + "</li>"
+
+      else {
+        outputTag = "<li class='green'>" + output + "</li>"
+      }
     }
 
     if (!output) {
-      output += i
+      outputTag = outputTag = "<li>" + i + "</li>"
     }
 
-    document.getElementById('js-results').innerHTML += output + "</br>";
+
+    document.getElementById('js-results').innerHTML += outputTag;
   }
 }
